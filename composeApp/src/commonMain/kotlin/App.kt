87089a20b3867f058edd6a4b25c2e3581cf3212a
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2024-04-16 19:12:32 -0300

    first commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,14 @@
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+val lightRedColor = Color(color = 0xFFF57D88)
+val darkRedColor = Color(color = 0xFF77000B)
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+    }
+}
